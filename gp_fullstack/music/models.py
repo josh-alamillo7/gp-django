ea61c8fb0ee6caf2170080bdcd167fc029c24d7f
Josh Alamillo <josh.alamillo@gmail.com>
2018-09-18 15:46:26 -0700

    Added data to database

--- gp_fullstack/music/models.py
+++ gp_fullstack/music/models.py
@@ -13,10 +13,10 @@ class Genre(models.Model):
 class Song(models.Model):
   title = models.CharField(max_length=200)
   genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-  artist = models.Artist(Artist, on_delete=models.CASCADE)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
   plays = models.PositiveIntegerField(default=0)
 
 class PlayCount(models.Model):
   song = models.ForeignKey(Song, on_delete=models.CASCADE)
-  user = models.User(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   plays = models.PositiveIntegerField(default=0)
