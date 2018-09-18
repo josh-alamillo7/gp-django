e252d63faa7f3e6b8396ff9a57a03b093a5b0dfd
Josh Alamillo <josh.alamillo@gmail.com>
2018-09-18 13:06:45 -0700

    Got MySql working

--- gp_fullstack/gp_fullstack/settings.py
+++ gp_fullstack/gp_fullstack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*&z5ohq87o1(#r9)k9lmz0n-b7&-mehzwu_m8g1-&kr12yx!7*'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'gp_fullstack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music',
+        'USER': 'josh-alamillo7',
+        'PASSWORD': config.DB_PW,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
