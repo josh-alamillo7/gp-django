3f2f9bd699ab4a21536226972f15faa21ed65a31
Josh Alamillo <josh.alamillo@gmail.com>
2018-09-18 13:29:56 -0700

    Added tables to mysql using settings file

--- gp_fullstack/gp_fullstack/settings.py
+++ gp_fullstack/gp_fullstack/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import config
+from .config import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'music',
         'USER': 'josh-alamillo7',
-        'PASSWORD': config.DB_PW,
+        'PASSWORD': config['DB_PW'],
         'HOST': 'localhost',
         'PORT': '',
     }
