8fae5ab92f3fc90163aa440535ec061c1af39ae2
Josh Alamillo <josh.alamillo@gmail.com>
2018-09-18 17:32:16 -0700

    Added in all the URLs I should need for the user interaction

--- gp_fullstack/music/urls.py
+++ gp_fullstack/music/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
   path('', views.index),
   path('userform', views.userform),
+  path('songs/<int:song_id>', views.songinfo),
+  path('artists/<int:artist_id>', views.artist),
+  path('genres/<int:genre_id>', views.genre),
 ]
