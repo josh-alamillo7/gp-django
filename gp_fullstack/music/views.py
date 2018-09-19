829879469249a48257cbe12082edae433d300ec0
Josh Alamillo <josh.alamillo@gmail.com>
2018-09-18 17:18:26 -0700

    Properly renders urls for home page and form page

--- gp_fullstack/music/views.py
+++ gp_fullstack/music/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+  return HttpResponse('Some placeholder index.. welcome page and buttons or something')
+  #Submit a song, browse songs by genre, browse songs by artist, get a music taste breakdown report.
+
+def userform(request):
+  return HttpResponse('User form will go here')
